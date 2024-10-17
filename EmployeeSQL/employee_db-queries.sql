3d9bb6548ec7de1ca26b5238ea5cee2354a987e7
jimmylee-123 <jimmylee123@gmail.com>
2024-10-16 21:15:48 -0700

    Added the correct "employee_db-queries.sql" file

--- EmployeeSQL/employee_db-queries.sql
+++ EmployeeSQL/employee_db-queries.sql
@@ -1,57 +1,47 @@
--- Create the Departments table
-CREATE TABLE departments (
-    dept_no VARCHAR(5) PRIMARY KEY,
-    dept_name VARCHAR(20) NOT NULL
-);
+-- List the employee number, last name, first name, sex, and salary of each employee.
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
 
--- Create the Titles table
-CREATE TABLE titles (
-    title_id VARCHAR(5) PRIMARY KEY,
-    title VARCHAR(20) NOT NULL
-);
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
--- Create the Employees table
-CREATE TABLE employees (
-    emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR(5) NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    sex CHAR(1) NOT NULL,
-    hire_date DATE NOT NULL,
-    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
-);
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM departments d
+JOIN dept_manager dm ON d.dept_no = dm.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
 
--- Create the Salaries table
-CREATE TABLE salaries (
-	emp_no INT NOT NULL,
-    salary INT NOT NULL,
-	PRIMARY KEY (emp_no),  --Composite primary key
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
+-- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
 
--- Create the Department Managers table
-CREATE TABLE dept_manager (
-	dept_no VARCHAR(5) NOT NULL,
-	emp_no INT NOT NULL,
-	PRIMARY KEY (dept_no, emp_no),  -- Composite primary key
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT first_name, last_name, gender
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
 
--- Create the Department Employees table
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR(5) NOT NULL,
-	PRIMARY KEY (emp_no, dept_no),  -- Composite primary key
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
-);
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
 
--- Show each table after importing data in order
---SELECT * FROM departments
---SELECT * FROM titles
---SELECT * FROM employees
---SELECT * FROM dept_manager
---SELECT * FROM dept_emp
---SELECT * FROM salaries
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name, COUNT(*) AS name_count
+FROM employees
+GROUP BY last_name
+ORDER BY name_count DESC;
